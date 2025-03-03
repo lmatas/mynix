9f56fa090744296879139058c263f2639ed02672
Lautaro Matas <lmatas@gmail.com>
2025-03-03 11:54:07 +0100

    first commit

---
+++ flake.nix
@@ -0,0 +1,179 @@
+{
+  description = "lmatas nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Home-manager
+    home-manager.url = "github:rycee/home-manager/master";
+
+    # Homebrew start
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+    homebrew-bundle = {
+      url = "github:homebrew/homebrew-bundle";
+      flake = false;
+    };
+    # Homebrew end
+
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, nix-homebrew
+    , homebrew-core, homebrew-cask, homebrew-bundle }:
+    let
+      configuration = { pkgs, config, ... }: {
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        environment.systemPackages = with pkgs; [
+
+          
+          neovim
+          mkalias
+          nixfmt-classic
+       
+          jdk11
+          maven
+
+          python312
+          python312Packages.pip
+
+          nodejs_20
+          yarn
+
+        ];
+
+        # nerd fonts
+        fonts.packages = [
+          pkgs.nerd-fonts.jetbrains-mono
+          pkgs.nerd-fonts.meslo-lg
+
+        ];
+
+        environment.variables = {
+          JAVA_HOME = "${pkgs.openjdk11}/zulu-11.jdk/Contents/Home";
+          PATH = "$JAVA_HOME/bin:$PATH";
+        };
+
+        # Add this section:
+        users.users.lmatas = { home = "/Users/lmatas"; };
+
+        # homebrew casks
+        homebrew = {
+          enable = true;
+          casks = [
+            "firefox"
+            "google-chrome"
+            "the-unarchiver"
+
+          ];
+          onActivation.cleanup = "zap";
+          onActivation.autoUpdate = true;
+          onActivation.upgrade = true;
+        };
+
+        # Set up applications in /Applications.
+        system.activationScripts.applications.text = let
+          env = pkgs.buildEnv {
+            name = "system-applications";
+            paths = config.environment.systemPackages;
+            pathsToLink = "/Applications";
+          };
+        in pkgs.lib.mkForce ''
+          # Set up applications.
+          echo "setting up /Applications..." >&2
+          rm -rf /Applications/Nix\ Apps
+          mkdir -p /Applications/Nix\ Apps
+          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          while read -r src; do
+            app_name=$(basename "$src")
+            echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+        '';
+
+        # Set up user mac system defaults.
+        system.defaults = {
+
+          dock.autohide = false;
+          finder.FXPreferredViewStyle = "clmv";
+
+        };
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 6;
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
+
+        # touchid in mac
+        security.pam.services.sudo_local.touchIdAuth = true;
+
+        # unfree 
+        nixpkgs.config.allowUnfree = true;
+
+      };
+    in {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#mini
+      darwinConfigurations."mini" =
+
+        nix-darwin.lib.darwinSystem {
+          modules = [
+            configuration
+
+            # Homebrew
+            nix-homebrew.darwinModules.nix-homebrew
+            {
+              nix-homebrew = {
+                # Install Homebrew under the default prefix
+                enable = true;
+
+                # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+                enableRosetta = true;
+
+                # User owning the Homebrew prefix
+                user = "lmatas";
+
+                # Optional: Declarative tap management
+                taps = {
+                  "homebrew/homebrew-core" = homebrew-core;
+                  "homebrew/homebrew-cask" = homebrew-cask;
+                  "homebrew/homebrew-bundle" = homebrew-bundle;
+                };
+
+                # Optional: Enable fully-declarative tap management
+                #
+                # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+                mutableTaps = false;
+              };
+            } # nix-homebrew
+
+            # Home Manager!  
+            home-manager.darwinModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.lmatas = { imports = [ ./home.nix ]; };
+            } # home-manager
+
+          ];
+        };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,68 @@
+# home.nix
+{ config, pkgs, ... }:
+
+{
+
+  home.stateVersion = "23.11"; # Or a different version if appropriate
+
+  #home.homeDirectory = pkgs.lib.mkDefault "/Users/lmatas"; # Use mkDefault!
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      terminal.shell = {
+        program = "${pkgs.tmux}/bin/tmux"; # Or /bin/zsh, etc.
+        args = [ "new-session" "-A" "-s" "default" ];
+      };
+
+      font = {
+        normal = {
+          family = "JetBrainsMono Nerd Font"; # Nombre de la fuente
+          style = "Regular"; # Estilo (Regular, Bold, Italic, etc.)
+        };
+        bold = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Bold";
+        };
+        italic = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Italic";
+        };
+        bold_italic = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Bold Italic";
+        };
+        size = 12.0; # Tamaño de la fuente (en puntos)
+      };
+    };
+  };
+
+  programs.tmux = { enable = true; };
+
+  programs.oh-my-posh = {
+    enable = true;
+    settings =
+      builtins.fromJSON (builtins.readFile ./oh-my-posh/atomic.omp.json);
+
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      nvim-treesitter.withAllGrammars
+      startup-nvim
+    ];
+  };
+
+  xdg.configFile."tmux/tmux.conf".source = ./tmux.conf;
+
+}
